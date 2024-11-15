c9752485784e8c02222fb5962fb01ab85f4d8c34
lse-7660 <lse76609251@gmail.com>
2024-11-15 16:08:13 +0900

    1115복습범위

---
+++ src/app/1115FRI/1115FRI.jsx
@@ -0,0 +1,8 @@
+// Input state: dashboard-study/state/page.jsx
+// quest 1: figure out why the input box was unusable
+// quest 2: figure out the reason we need to use useState while we can use text input boxs without it
+
+//Router, Nav: components/layout/Header.jsx
+
+// Class 문법: FRONTEND-1/04-js/07-class
+// 생성자함수, 매서드: https://odada.me/247
