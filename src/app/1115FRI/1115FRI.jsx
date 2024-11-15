3734b89b14b3750d424860b6c5c34bdfccc3b922
lse-7660 <lse76609251@gmail.com>
2024-11-15 17:51:16 +0900

    1115-복습중

--- src/app/1115FRI/1115FRI.jsx
+++ src/app/1115FRI/1115FRI.jsx
@@ -1,6 +1,7 @@
 // Input state: dashboard-study/state/page.jsx
 // quest 1: figure out why the input box was unusable
 // quest 2: figure out the reason we need to use useState while we can use text input boxs without it
+// quest 2(chatGPT): useState를 통해 input을 관리하면 입력값의 길이를 제한하거나, 특정 조건에 따라 입력값을 수정 또는 무효화할 수 있다; 즉, 단순 화면 표시가 아니라 "입력된 값을 활용해 동작을 제어"할 수 있다
 
 //Router, Nav: components/layout/Header.jsx
 
