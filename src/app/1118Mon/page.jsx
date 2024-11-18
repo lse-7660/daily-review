1ee26387e69d04c0bc65426c95804893b21a39bf
lse-7660 <lse76609251@gmail.com>
2024-11-18 17:47:55 +0900

    1118-수업끗

---
+++ src/app/1118Mon/page.jsx
@@ -0,0 +1,25 @@
+// class 함수 복습
+// 리터럴 배열: const a = [1, 2, 3, 4]
+// 생성자함수: const b = new Array('a', 'b', 'c', 'd')
+// 생성자 함수는 new 연산자를 통해 객체를 생성하는 함수다
+// 객체 내부에 있는 함수가 메서드임
+// 매서드 만드는 거랑 펑션 쓰는거랑 머가 다른거임
+// function OrderCoffee(name, price) {this.name = name; this.price.price;}
+// ㄴ생성자함수만듬
+// OrderCoffee 생성자 함수 프로토타입에 order 메소드를 생성
+// function OrderCoffee.prototype.order = function(){}
+// 근데 이건 옛날 방식이고, 이제는 class함수 씀
+
+// class 함수
+// constructor로 객체 생성(생성자 함수 역할을 함)
+// 쓰는 방법은 메서드랑 똑같음
+
+// curri > js > 클래스(class) > 5.class 상속부터!
+// 리액트에서 클래스 사용하기: dashboard > app > class
+
+// 표준내장객체: curri > js > 표준 내장 객체-문자열
+// 파일: frontend > js > 08
+
+// 무한바보생성기 만들기
+// 버튼 누르면 그 자리에 바보 출력되고 버튼 랜덤한 위치로 감
+// 그걸 무한반복
