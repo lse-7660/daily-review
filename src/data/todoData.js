044c9c2e20060ed25f3437b3a33cf0ceea7ea4c4
lse-7660 <lse76609251@gmail.com>
2024-11-21 22:18:31 +0900

    1121-완

---
+++ src/data/todoData.js
@@ -0,0 +1,20 @@
+export const mockTodoData = [
+    {
+        id: 1,
+        isDone: false,
+        task: 'making Todo List',
+        createdDate: '2024.11.18',
+    },
+    {
+        id: 2,
+        isDone: false,
+        task: 'lesson review',
+        createdDate: '2024.11.19',
+    },
+    {
+        id: 3,
+        isDone: false,
+        task: 'drawing Lucker',
+        createdDate: '2024.11.21',
+    },
+];
