acbe8a448e75bc62fdaec8c10d03e6fc826a92a7
lse-7660 <lse76609251@gmail.com>
2024-11-25 17:04:54 +0900

    1125Mon-수업

---
+++ src/app/1125Mon/page.jsx
@@ -0,0 +1,15 @@
+// 한시간 못들음 예라이~
+// Todo Reducer부터 보면 되는듯, 일단 강사님 깃허브 받고 다음꺼부터 듣는다
+// 어차피 지난주꺼 리뷰해야되는거
+// 에휴 시벌
+// hooks)reducer도 같이 해
+// npm install classnames
+// 액션 타입 정의
+// Hooks)useContext
+// 공부할거 많다~ 이런 미친!~
+// ㅋ ㅅㅂ,,,
+
+// ui 라이브러리
+// chakra ui
+// mui:안드로이드디자인형태, 대기업에서 많이 씀
+// ant design: 중국꺼, 전세계적으로 많이 씀
