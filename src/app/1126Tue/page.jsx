c4d941f38b00273c84d6c42f2b7f3b1c81749b97
lse-7660 <lse76609251@gmail.com>
2024-11-26 17:50:27 +0900

    1126Tue-수업끗

---
+++ src/app/1126Tue/page.jsx
@@ -0,0 +1,7 @@
+// useContext 다시
+// 이걸 왜 하냐?! 자식한테전달>자식한테전달>자식한테전달... 하지않고 전역으로 관리하려고
+// 외부에 따로 만든 Context 파일에 mockupData/ useState / useEffect 만들고 createContext한 뒤에 사용해야할 파일 전체(Layout) Provider로 싸줌 > Context 파일에 있는 요소들 ㄷ ㅏ쓸수있대
+// dashboard에다가 like 버튼을 만들어봅시다
+// 기능 왜 안되는지 지피티한테 물어보기,,,,,하,,,,
+// 최적화
+// useMemo는 !!!! 불필요한 연산!!!!!ㄹ을 반복하지 ㅇ낳게 해줌!!!
